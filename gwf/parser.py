a999584fed49ac3772e69293b1b0f515568efff4
Thomas Mailund <mailund@birc.au.dk>
2013-06-22 19:06:07 +0200

    Checking dependencies on files...

--- gwf/parser.py
+++ gwf/parser.py
@@ -1,9 +1,14 @@
 '''Functionality for parsing a workflow specification file.'''
 
+import os.path
 import re
 from workflow import Target, Workflow
 
-def parse_target(target_code):
+# working dir is provided to the parse in case we want to allow
+# recursive workflows or something in the future... and I need it for
+# the files in Targets() at some point, and I might as well provide it
+# here rather than setting it in the workflow class later...
+def parse_target(target_code, working_dir):
     lines = target_code.split('\n')
     _, name = lines[0].split()
 
@@ -30,13 +35,15 @@ def parse_target(target_code):
             code = '\n'.join(lines[i:])
             break
                 
-    return Target(name, input, output, code)
+    return Target(name, input, output, code, working_dir)
 
 PARSERS = {'target': parse_target}
 
 def parse(fname):
     '''Parse up the workflow in "fname".'''
     # FIXME: add error handling!
+
+    working_dir = os.path.dirname(os.path.realpath(fname))
     workflow_text = open(fname).read()
     commands = workflow_text.split('\n@')[1:]
 
@@ -45,7 +52,7 @@ def parse(fname):
         opcode,_ = re.split('\s',cmd,1)
         assert opcode in PARSERS # FIXME: add error handling
 
-        workflow.append(PARSERS[opcode](cmd))
+        workflow.append(PARSERS[opcode](cmd, working_dir))
 
     targets = dict()
     for cmd in workflow:
--- gwf/workflow.py
+++ gwf/workflow.py
@@ -2,14 +2,23 @@
 
 # FIXME: handle errors
 
+import os.path, time
+
+def _file_exists(fname):
+    return os.path.exists(fname)
+
+def _get_file_timestamp(fname):
+    return time.ctime(os.path.getmtime(fname))
+
 class Target:
     '''Class handling targets. Stores the info for executing them.'''
     
-    def __init__(self, name, input, output, code):
+    def __init__(self, name, input, output, code, wd):
         self.name = name
         self.input = input
         self.output = output
         self.code = code
+        self.working_dir = wd
 
         # dependencies are the targets that provide files.
         self.dependencies = None # will be filled in by Workflow
@@ -17,6 +26,50 @@ class Target:
         # system_files are expected to be existing, since
         # we don't have targets providing them.
         self.system_files = None # will be filled in by Workflow
+
+    def check_system_files(self):
+        '''Make sure that the files we don't know how to make are
+        actually on the file server.'''
+
+        for sysfile in self.system_files:
+           assert _file_exists(self.working_dir+'/'+sysfile), \
+           "There is no target providing %s. It must be present!" % sysfile
+
+    def should_run(self):
+        '''Test if this target needs to be run.'''
+        
+        if len(self.output) == 0:
+            return True # If we don't provide output, assume we always
+                        # need to run.
+        if len(self.input) == 0:
+            return True # If we don't have any input, we also assume
+                        # we have to run
+
+        # If we have both input and output, we need to run if any of
+        # the input files are newer than any of the output
+        # files... and if any of the output or inputs are missing, we
+        # definitely have to run.
+        
+        for outf in self.output:
+            # FIXME: Handle absolute paths...
+            if not _file_exists(self.working_dir+'/'+outf):
+                return True
+
+        for inf in self.input:
+            if not _file_exists(self.working_dir+'/'+inf):
+                # FIXME: error handling
+                assert inf not in self.system_files, \
+                    "There is no target providing %s. It must be present!" % inf
+                return True
+
+        # if we have all input and output files, check time stamps
+        in_timestamp = min(_get_file_timestamp(self.working_dir+'/'+inf)
+                           for inf in self.input)
+        out_timestamp = min(_get_file_timestamp(self.working_dir+'/'+outf)
+                            for outf in self.output)
+
+        return in_timestamp > out_timestamp
+
 
     def __str__(self):
         sf = ''
@@ -27,14 +80,15 @@ class Target:
         if self.dependencies:
             dep = ','.join([('target %s should provide %s ' % (tg.name,of))
                             for of,tg in self.dependencies])
-        return '@target %s, input(%s) -> output(%s), %s [%s; %s]' % (
+        return '@target %s, input(%s) -> output(%s), %s [%s; %s] %s' % (
             self.name,
             ' '.join(self.input),
             ' '.join(self.output),
             '%d lines of code' % len(self.code.split('\n')),
-            sf, dep
+            sf, dep,
+            'Should run' if self.should_run() else "Doesn't need to run"
             )
-    __repr__ = __str__
+    __repr__ = __str__ # not really the correct use of __repr__ but easy for printing output when testing...
 
 
 class Workflow:
