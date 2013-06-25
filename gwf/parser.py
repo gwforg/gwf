cf0b101892b18dd5b1405c4bbc96e117c3444144
Thomas Mailund <mailund@birc.au.dk>
2013-06-25 19:48:36 +0200

    Slightly better error handling in the parser.

--- gwf/parser.py
+++ gwf/parser.py
@@ -33,8 +33,9 @@ def parse_target(target_code, working_dir):
                 pbs.append(' '.join(options))
 
             else:
-                assert False, 'Unknown opcode %s' % line
-                # FIXME: Proper error handling
+                print "Unknown opcode %s in target %s." % (line, name)
+                sys.exit(2)
+
         else:
             # assumes everything after the opcodes is the script
             break
@@ -47,7 +48,6 @@ PARSERS = {'target': parse_target}
 
 def parse(fname):
     '''Parse up the workflow in "fname".'''
-    # FIXME: add error handling!
 
     working_dir = os.path.dirname(os.path.realpath(fname))
     workflow_text = open(fname).read()
@@ -56,27 +56,23 @@ def parse(fname):
     workflow = []
     for cmd in commands:
         opcode,_ = re.split('\s',cmd,1)
-        assert opcode in PARSERS # FIXME: add error handling
+        
+        if opcode not in PARSERS:
+            print 'Unknown task type @%s.' % opcode
+            sys.exit(2)
 
         workflow.append(PARSERS[opcode](cmd, working_dir))
 
     targets = dict()
     for cmd in workflow:
+        # FIXME: probably shouldn't hardwire a test for the type of task here!
         if isinstance(cmd, Target):
-            # FIXME: error handling
-            assert cmd.name not in targets
+
+            if cmd.name in targets:
+                print 'Task %s appears more than once in the workflow.' % cmd.name
+                sys.exit(2)
+            
             targets[cmd.name] = cmd
 
     return Workflow(targets, working_dir)
 
-if __name__ == '__main__':
-    # FIXME: make a better test...
-    import sys
-    workflow = parse(sys.argv[1])
-
-    print 'Workflow contains %d targets:' % len(workflow.targets)
-    print
-    for name,target in workflow.targets.items():
-        print target
-        target.get_dependencies().print_dependency_graph()
-        print
