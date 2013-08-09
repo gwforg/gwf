b928e7a854f98315444f27b5983f3fa9fdfbbfb0
Thomas Mailund <mailund@birc.au.dk>
2013-08-09 10:41:37 +0200

    Collecting existing output files

--- gwf/workflow.py
+++ gwf/workflow.py
@@ -449,6 +449,15 @@ class Target(ExecutableTask):
         else:
             return None
 
+    def get_existing_outfiles(self):
+        '''Get list of output files that already exists.'''
+        result = []
+        for outf in self.output:
+            fname = _make_absolute_path(self.working_dir, outf)
+            if _file_exists(fname):
+                result.append(fname)
+        return result
+
     def __str__(self):
         return '@target %s, input(%s) -> output(%s)' % (
             self.name,
