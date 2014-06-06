e2e367b6a0e7f785006078c34c56c8ea75156886
Thomas Mailund <mailund@birc.au.dk>
2014-06-06 13:34:35 +0200

    Script for moving from old to new job handling

---
+++ port-job-status.py
@@ -0,0 +1,37 @@
+"""
+Script to move job status information from the old .jobs directory to a database.
+
+It doesn't do this recursively based on a workflow but only a single .jobs/ directory, so be careful
+when porting a running workflow!
+"""
+
+import os.path
+from os import listdir
+import sys
+import shelve
+
+if len(sys.argv) > 1:
+    workflow_dir = sys.argv[1]
+else:
+    workflow_dir = '.'
+
+jobs_dir = os.path.join(workflow_dir, '.jobs')
+jobs = listdir(jobs_dir)
+jobs_db = shelve.open(os.path.join(workflow_dir, '.jobs'))
+
+for job_name in jobs:
+    job_file = os.path.join(jobs_dir, job_name)
+    print 'job file for', job_name, job_file
+
+    try:
+        job_id = int(open(job_file).read().strip())
+        print 'job ID', job_id
+        jobs_db[job_name] = job_id
+    except:
+        pass
+
+jobs_db.close()
+
+jobs_db = shelve.open(os.path.join(workflow_dir, '.jobs'))
+for job_name in jobs_db:
+    print job_name, jobs_db[job_name]
--- src/gwf_workflow/workflow.py
+++ src/gwf_workflow/workflow.py
@@ -120,8 +120,7 @@ class Node(object):
         # The youngest in should be older than the oldest out
         if youngest_in_timestamp >= oldest_out_timestamp:
             # we have a younger in file than an outfile
-            self.reason_to_run = 'Infile %s is younger than outfile %s' % \
-                                 (youngest_in_filename, oldest_out_filename)
+            self.reason_to_run = 'Infile %s is younger than outfile %s' %  (youngest_in_filename, oldest_out_filename)
             self.cached_should_run = True
             return True
         else:
