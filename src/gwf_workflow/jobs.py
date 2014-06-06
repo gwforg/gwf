f12a28806e0c590f0961036e5c604b70084e86f2
Thomas Mailund <mailund@birc.au.dk>
2014-06-06 17:01:13 +0200

    Removed debug output

--- src/gwf_workflow/jobs.py
+++ src/gwf_workflow/jobs.py
@@ -35,7 +35,6 @@ class JobsDatabase(object):
         for job_name in self.db:
             job_id = self.db[job_name]
             job_status = get_job_status(job_id)
-            print job_name
             if job_status is None:
                 # It is no longer in the queue so it shouldn't be in the jobs db
                 del self.db[job_name]
