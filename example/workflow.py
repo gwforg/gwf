9089e359caf4f86c6adb978567bcb05e09826155
Thomas Mailund <mailund@birc.au.dk>
2014-07-10 09:40:54 +0200

    fixed named parameters

--- example/workflow.py
+++ example/workflow.py
@@ -7,6 +7,6 @@ from gwf.samtools import samtools_sort
 
 
 target('IndexGenome') << bwa_index(refGenome='ponAbe2')
-target('MapReads')    << bwa_map(refGenome='ponAbe2', name='Masala')
+target('MapReads')    << bwa_map(refGenome='ponAbe2', R1='Masala_R1.fastq.gz', R2='Masala_R2.fastq.gz', bamfile='Masala.unsorted.bam')
 target('SortBAM')     << samtools_sort(name='Masala')
 
