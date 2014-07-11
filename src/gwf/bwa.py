93e52726c76890b789eab8aad8eea4b826f438e6
Thomas Mailund <mailund@birc.au.dk>
2014-07-11 10:34:02 +0200

    Added a FIXME

--- src/gwf/bwa.py
+++ src/gwf/bwa.py
@@ -3,7 +3,8 @@ from gwf import *
 bwa_index = template(input='{refGenome}.fa', 
                      output=['{refGenome}.amb', '{refGenome}.ann', '{refGenome}.pac']) \
     << 'bwa index -p {refGenome} -a bwtsw {refGenome}.fa'
-                     
+
+# FIXME: This doesn't work on slurm where the PBS_JOBID is called SLURM_JOBID. Deal with that!
 bwa_map = template(input=['{R1}', '{R2}', '{refGenome}.amb', '{refGenome}.ann', '{refGenome}.pac'],
                    output='{bamfile}',
                    pbs='-l nodes=1:ppn=16') << '''
