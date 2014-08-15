184cb24bbb594c3f278832101217720577cde689
Thomas Mailund <mailund@birc.au.dk>
2014-08-15 10:31:34 +0200

    Copied code from Anders

--- src/gwf_workflow/jobs.py
+++ src/gwf_workflow/jobs.py
@@ -5,18 +5,53 @@ Module for keeping track of job status for jobs in the grid queue.
 import shelve
 import subprocess
 import os.path
+from itertools import izip
 
 
-def get_job_status(job_id):
+def get_state_of_jobs_slurm(job_ids):
+    result = dict((job_id, False) for job_id in job_ids)
+    map_state = {  # see squeue man page under JOB STATE CODES
+                   'BF': '?',  # BOOT_FAIL
+                   'CA': '?',  # CANCELLED
+                   'CD': '?',  # COMPLETED
+                   'CF': 'R',  # CONFIGURING
+                   'CG': 'R',  # COMPLETING
+                   'F':  '?',  # FAILED
+                   'NF': '?',  # NODE_FAIL
+                   'PD': 'Q',  # PENDING
+                   'PR': '?',  # PREEMPTED
+                   'R':  'R',  # RUNNING
+                   'S':  'R',  # SUSPENDED
+                   'TO': '?',  # TIMEOUT
+                   'SE': 'Q',  # SPECIAL_EXIT
+    }
     try:
-        stat = subprocess.Popen(['qstat', '-f', job_id], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stat = subprocess.Popen(['squeue',
+                                 '--noheader',
+                                 '--format=%i;%t',
+                                 '--jobs', ",".join(job_ids)],
+                                stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         for line in stat.stdout:
-            line = line.strip()
-            if line.startswith('job_state'):
-                status = line.split()[2]
-                return status
+            job_id, state = line.strip().split(';')
+            result[job_id] = map_state[state]
     except:
-        return False
+        pass
+    return result
+
+
+def get_state_of_jobs_torque(job_ids):
+    result = dict((job_id, None) for job_id in job_ids)
+    for job_id in job_ids:
+        try:
+            stat = subprocess.Popen(['qstat', '-f', job_id], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            for line in stat.stdout:
+                line = line.strip()
+                if line.startswith('job_state'):
+                    status = line.split()[2]
+                    result[job_id] = status
+        except:
+            pass
+    return result
 
 
 def make_db_file_name(workflow_directory):
@@ -32,9 +67,12 @@ class JobsDatabase(object):
         self._read_and_update_status()
 
     def _read_and_update_status(self):
-        for job_name in self.db:
+        job_names = list(self.db.keys())
+        job_ids = list(self.db[name] for name in job_names)
+        current_state = get_state_of_jobs_torque(job_ids)
+        for job_name, job_id in izip(job_names, job_ids):
             job_id = self.db[job_name]
-            job_status = get_job_status(job_id)
+            job_status = current_state[job_id]
             if job_status not in ('Q', 'R', 'H'):
                 # It is no longer in the queue so it shouldn't be in the jobs db
                 del self.db[job_name]
@@ -44,7 +82,7 @@ class JobsDatabase(object):
 
     def set_job_id(self, target_name, job_id):
         self.db[target_name] = job_id
-        self.status_db[target_name] = 'Q'      # It starts out as a queued object...
+        self.status_db[target_name] = 'Q'  # It starts out as a queued object...
 
     def in_queue(self, job_name):
         return job_name in self.status_db
@@ -88,6 +126,8 @@ JOBS_QUEUE = JobsDBCollection()
 
 # Necessary to close all databases at exit...
 import atexit
+
+
 @atexit.register
 def close_databases():
     JOBS_QUEUE.close()
