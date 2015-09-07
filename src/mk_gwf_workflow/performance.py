6e5aafed278754d18b9709509a57d3f833e38fc3
Michael Knudsen <michaelk@clin.au.dk>
2015-09-07 10:34:42 +0200

    Interges are better then floats when representing seconds...

--- src/mk_gwf_workflow/performance.py
+++ src/mk_gwf_workflow/performance.py
@@ -2,17 +2,12 @@ import re
 
 def job_cpu_performance(job):
 
-	used_cpu_time = job['used_cpu_time']
-	used_walltime = job['used_walltime']
+	cores = int(job['cores'])
 
-	print used_cpu_time, time_in_seconds(used_cpu_time)
-	print used_walltime, time_in_seconds(used_walltime)
+	used_cpu_time = time_in_seconds(job['used_cpu_time'])
+	used_walltime = time_in_seconds(job['used_walltime'])
 
-	if used_cpu_time == '--':
-		return 'NA'
-
-	else:
-		print job['id'], used_cpu_time, used_walltime
+	
 
 def job_memory_performace(job):
 
@@ -24,11 +19,11 @@ def time_in_seconds(time):
 	time_parts = time_parts_re.match(time)
 
 	if time_parts == None:
-		return 0.0
+		return 0
 
-	seconds = float(time_parts.group('seconds'))
-	minutes	= float(time_parts.group('minutes'))
-	hours	= float(time_parts.group('hours') or '0.0')
-	days 	= float(time_parts.group('days') or '0.0')
+	seconds = int(time_parts.group('seconds'))
+	minutes	= int(time_parts.group('minutes'))
+	hours	= int(time_parts.group('hours') or '0.0')
+	days 	= int(time_parts.group('days') or '0.0')
 
 	return seconds + 60 * minutes + 60 * 60 * hours + 24 * 60 * 60 * days
