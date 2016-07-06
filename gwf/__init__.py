354a1bdbb3efb7063b9cdfe326b00590bf1b2584
Dan Søndergaard <dan1990@gmail.com>
2016-07-06 16:03:55 +0200

    Oops, remove some debugging code...

--- gwf/__init__.py
+++ gwf/__init__.py
@@ -5,7 +5,3 @@ from gwf.api import target, template, memorize, include_workflow, shell, glob
 
 
 __all__ = ['target', 'template', 'memorize', 'include_workflow', 'shell', 'glob']
-
-
-if __name__ == '__main__':
-    pass
