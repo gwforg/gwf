ba000ab838a8cb7b6c2226ca3d20083395120c75
Dan Søndergaard <dan1990@gmail.com>
2016-08-10 10:36:16 +0200

    Get ready for testing.

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,12 +1,25 @@
 import unittest
 
+from gwf import Workflow, Target
+
 
 class TestWorkflow(unittest.TestCase):
     pass
 
 
 class TestTarget(unittest.TestCase):
-    pass
+
+    def test_input_paths_are_normalized(self):
+        self.assertTrue(False)
+
+    def test_output_paths_are_normalized(self):
+        self.assertTrue(False)
+
+    def test_target_without_outputs_is_a_sink(self):
+        self.assertTrue(False)
+
+    def test_target_without_inputs_is_a_source(self):
+        self.assertTrue(False)
 
 
 class TestDependencies(unittest.TestCase):
