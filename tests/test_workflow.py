2f13046bb7a662a33769966a7bd7a6162ac94d5c
Dan Søndergaard <dan1990@gmail.com>
2016-08-10 11:03:20 +0200

    Initial tests for Target.

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,3 +1,4 @@
+import os.path
 import unittest
 
 from gwf import Workflow, Target
@@ -9,17 +10,83 @@ class TestWorkflow(unittest.TestCase):
 
 class TestTarget(unittest.TestCase):
 
-    def test_input_paths_are_normalized(self):
+    def test_relative_input_paths_are_normalized(self):
+        target = Target(name='TestTarget',
+                        inputs=['test_input1.txt', 'test_input2.txt'],
+                        outputs=[],
+                        options={},
+                        working_dir='/some/path')
+
+        self.assertTrue(os.path.isabs(target.inputs[0]))
+        self.assertTrue(os.path.isabs(target.inputs[1]))
+
+        self.assertTrue(target.inputs[0].startswith('/some/path'))
+        self.assertTrue(target.inputs[1].startswith('/some/path'))
+
+    def test_relative_output_paths_are_normalized(self):
+        target = Target(name='TestTarget',
+                        inputs=[],
+                        outputs=['test_output1.txt', 'test_output2.txt'],
+                        options={},
+                        working_dir='/some/path')
+
+        self.assertTrue(os.path.isabs(target.outputs[0]))
+        self.assertTrue(os.path.isabs(target.outputs[1]))
+
+        self.assertTrue(target.outputs[0].startswith('/some/path'))
+        self.assertTrue(target.outputs[1].startswith('/some/path'))
+
+    def test_absolute_input_paths_are_not_normalized(self):
         self.assertTrue(False)
 
-    def test_output_paths_are_normalized(self):
+    def test_absolute_output_paths_are_not_normalized(self):
         self.assertTrue(False)
 
     def test_target_without_outputs_is_a_sink(self):
-        self.assertTrue(False)
+        target = Target(name='TestTarget',
+                        inputs=[],
+                        outputs=[],
+                        options={},
+                        working_dir='')
+
+        self.assertTrue(target.is_sink)
+
+    def test_target_with_outputs_is_not_a_sink(self):
+        target = Target(name='TestTarget',
+                        inputs=[],
+                        outputs=['test_output1.txt', 'test_output2.txt'],
+                        options={},
+                        working_dir='')
+
+        self.assertFalse(target.is_sink)
 
     def test_target_without_inputs_is_a_source(self):
-        self.assertTrue(False)
+        target = Target(name='TestTarget',
+                        inputs=[],
+                        outputs=[],
+                        options={},
+                        working_dir='')
+
+        self.assertTrue(target.is_source)
+
+    def test_target_with_inputs_is_not_a_source(self):
+        target = Target(name='TestTarget',
+                        inputs=['test_input1.txt', 'test_input2.txt'],
+                        outputs=[],
+                        options={},
+                        working_dir='')
+
+        self.assertFalse(target.is_source)
+
+    def test_assigning_spec_to_target_sets_spec_attribute(self):
+        target = Target(name='TestTarget',
+                        inputs=[],
+                        outputs=[],
+                        options={},
+                        working_dir='') << 'this is a spec'
+
+        self.assertIsNotNone(target.spec)
+        self.assertEqual(target.spec, 'this is a spec')
 
 
 class TestDependencies(unittest.TestCase):
