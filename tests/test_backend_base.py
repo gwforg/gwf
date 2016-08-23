c65b47c9392b0d96d560a65fa47cb4e1a1664ba8
Dan Søndergaard <dan1990@gmail.com>
2016-08-23 12:26:10 +0200

    Fix backend tests.

--- tests/test_backend_base.py
+++ tests/test_backend_base.py
@@ -9,11 +9,11 @@ class TestBaseBackend(unittest.TestCase):
 
     def test_subclass_of_Backend_is_registered_in_backend_registry(self):
         class TestingBackend(Backend):
-            pass
+            name = 'testing'
 
         backends = get_backends()
-        self.assertIn('TestingBackend', backends)
-        self.assertEqual(TestingBackend, backends['TestingBackend'])
+        self.assertIn('testing', backends)
+        self.assertEqual(TestingBackend, backends['testing'])
 
     def test_backend_raises_exception_when_initialized_with_unprepared_workflow(self):
         with self.assertRaises(WorkflowNotPreparedError):
