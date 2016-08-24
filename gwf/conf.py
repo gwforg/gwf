6cb3bf873c7c186725a2563d5c7215cb5d817aec
Dan Søndergaard <dan1990@gmail.com>
2016-08-24 11:42:56 +0200

    Fix conf module.

--- gwf/conf.py
+++ gwf/conf.py
@@ -1,10 +1,10 @@
 """Handling of configuration files."""
 
 import os.path
-from ConfigParser import SafeConfigParser
+from configparser import SafeConfigParser
 
 USER_CONFIG_FILE = os.path.expanduser("~/.gwfrc")
 LOCAL_CONFIG_FILE = '.gwfrc'
 
-CONFIG_PARSER = SafeConfigParser()
-settings = CONFIG_PARSER.read([LOCAL_CONFIG_FILE, USER_CONFIG_FILE])
+settings = SafeConfigParser()
+settings.read([LOCAL_CONFIG_FILE, USER_CONFIG_FILE])
