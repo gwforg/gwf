457ef0ba12ea38a2e00d67048a32e123c75bfc4a
Dan Søndergaard <dan1990@gmail.com>
2016-08-25 15:30:32 +0200

    Small styling fix.

--- tests/test_backend_base.py
+++ tests/test_backend_base.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 from gwf.backends.base import Backend, get_backends
 from gwf.core import PreparedWorkflow, Workflow
