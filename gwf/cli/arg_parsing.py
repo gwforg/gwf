16d261d99fc78763dc023a5b31af1e1c7069bc6c
Thomas Mailund <mailund@birc.au.dk>
2016-09-14 12:41:16 +0200

    Removed default subcommand -- it is not going to work anyway.

--- gwf/cli/arg_parsing.py
+++ gwf/cli/arg_parsing.py
@@ -2,37 +2,6 @@ from __future__ import absolute_import, print_function
 from ..utils import cache, import_object
 from ..core import PreparedWorkflow
 import argparse
-import sys
-
-## Set default sub-command. Taken from https://bitbucket.org/ruamel/std.argparse
-def set_default_subparser(self, name, args=None):
-    """default subparser selection. Call after setup, just before parse_args()
-    name: is the name of the subparser to call by default
-    args: if set is the argument list handed to parse_args()
-
-    , tested with 2.7, 3.2, 3.3, 3.4
-    it works with 2.6 assuming argparse is installed
-    """
-    subparser_found = False
-    for arg in sys.argv[1:]:
-        if arg in ['-h', '--help']:  # global help if no subparser
-            break
-    else:
-        for x in self._subparsers._actions:
-            if not isinstance(x, argparse._SubParsersAction):
-                continue
-            for sp_name in x._name_parser_map.keys():
-                if sp_name in sys.argv[1:]:
-                    subparser_found = True
-        if not subparser_found:
-            # insert default in first position, this implies no
-            # global options without a sub_parsers specified
-            if args is None:
-                sys.argv.insert(1, name)
-            else:
-                args.insert(0, name)
-
-argparse.ArgumentParser.set_default_subparser = set_default_subparser
 
 
 class SubCommand(object):
@@ -81,9 +50,6 @@ class ArgumentDispatching(object):
         subparser = self.subparsers.add_parser(name, help = description)
         handler.set_arguments(subparser)
         subparser.set_defaults(func = handler.handle)
-
-    def set_default_command(self, name):
-        self.parser.set_default_subparser(name)
 
     def dispatch(self, argv):
         """Parse command line arguments in argv and dispatch to subcommand."""
