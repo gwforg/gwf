4bd7f20390653c6e1222657bf08999476ba34aa8
Dan Søndergaard <dan1990@gmail.com>
2016-09-26 08:46:41 +0200

    Put extension tests in subdirs.

---
+++ tests/plugins/test_base.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class BasePluginTest(unittest.TestCase):
+
+    def test_bar(self):
+        self.assertTrue(True)
---
+++ tests/plugins/test_run.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class RunPluginTest(unittest.TestCase):
+
+    def test_bar(self):
+        self.assertTrue(True)
