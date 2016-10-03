cfb69f96ff603eac15b3cb4118cf5777196378b5
Dan Søndergaard <dan1990@gmail.com>
2016-10-03 11:36:58 +0200

    Documentation for a few extension methods.

--- gwf/ext.py
+++ gwf/ext.py
@@ -31,6 +31,39 @@ class Extension(abc.ABC):
         """
 
     def setup_subparser(self, subparsers, name, description, handler):
+        """Helper method for setting up subparsers.
+
+        :param subparsers:
+            A subparsers object as returned by
+            :func:`ArgumentParser.add_subparsers`.
+        :param name:
+            Name of the subcommand.
+        :param description:
+            A short description of the subcommand.
+        :param handler:
+            The callable that will be called when the subcommand is used.
+
+        This method is most often used in :func:`setup_argument_parser` to set
+        up subcommands. For example, to register a subcommand called ``foo``
+        that calls the method ``on_foo``::
+
+            class MyPlugin(Plugin):
+                name = 'myplugin'
+
+                def setup_argument_parser(self, parser, subparsers):
+                    self.setup_subparser(
+                        subparsers,
+                        'foo',
+                        'a command that prints a greeting.',
+                        self.on_foo,
+                    )
+
+                def on_foo(self):
+                    print('Hello!')
+
+        When the plugin is installed and registered properly, the user will now
+        be able to run ``gwf foo`` which will print ``Hello!`` and exit.
+        """
         subparser = subparsers.add_parser(name, description=description)
         subparser.set_defaults(func=handler)
         return subparser
--- gwf/plugins/base.py
+++ gwf/plugins/base.py
@@ -5,6 +5,11 @@ class Plugin(Extension):
     """Abstract base class for plugins."""
 
     def configure(self, workflow, backend, config):
+        """Called to configure the plugin.
+
+        This method sets the workflow, backend and config attributes on the
+        object, but may be overridden to perform other kinds of initialization.
+        """
         self.workflow = workflow
         self.backend = backend
         self.config = config
