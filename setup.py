9ffa5e5c69e8a58bcfa5d18d7a142f690da9e4db
Dan Søndergaard <dan1990@gmail.com>
2016-10-11 12:42:36 +0200

    Use freakin' single quotes...

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name="gwf",
-    version="1.0.0",
+    name='gwf',
+    version='1.0.0',
 
     packages=find_packages(),
     entry_points={
@@ -38,26 +38,26 @@ setup(
 
     test_suite='tests',
     install_requires=[
-        "colorama>=0.3.7",
-        "configargparse>=0.11",
-        "statusbar>=0.1.2",
+        'colorama>=0.3.7',
+        'configargparse>=0.11',
+        'statusbar>=0.1.2',
     ],
 
     # metadata for upload to PyPI
-    author="Thomas Mailund, Dan Søndergaard",
-    author_email="mailund@birc.au.dk, das@birc.au.dk",
-    license="GPLv3",
-    keywords="grid computing workflow",
-    url="https://mailund.github.io/gwf",
-    description="A flexible, pragmatic workflow tool.",
+    author='Thomas Mailund, Dan Søndergaard',
+    author_email='mailund@birc.au.dk, das@birc.au.dk',
+    license='GPLv3',
+    keywords='grid computing workflow',
+    url='https://mailund.github.io/gwf',
+    description='A flexible, pragmatic workflow tool.',
     long_description=read('README.rst'),
 
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Topic :: Utilities",
-        "Topic :: System :: Distributed Computing",
-        "Intended Audience :: Science/Research",
-        "Programming Language :: Python",
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Topic :: Utilities',
+        'Topic :: System :: Distributed Computing',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
     ],
 )
