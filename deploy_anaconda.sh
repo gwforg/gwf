651cd2ed97acc86d8ca7bfb02f79174c1b225a9e
Dan Søndergaard <dan1990@gmail.com>
2017-01-10 11:09:31 +0100

    Fix deploy script.

--- deploy_anaconda.sh
+++ deploy_anaconda.sh
@@ -6,7 +6,7 @@ echo "Converting conda package..."
 conda convert --platform all $HOME/miniconda3/conda-bld/linux-64/gwf-*.tar.bz2 --output-dir conda-bld/
 
 echo "Deploying to Anaconda.org..."
-anaconda -t $ANACONDA_TOKEN upload conda-bld/**/PACKAGENAME-*.tar.bz2
+anaconda -t $ANACONDA_TOKEN upload conda-bld/**/gwf-*.tar.bz2
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
