7a8336c94d7c9cf96a84036237e62cdfe57c7871
Dan Søndergaard <dan1990@gmail.com>
2017-01-19 13:56:47 +0100

    Fix script for building conda packages. Hopefully works now.

--- build_conda.sh
+++ build_conda.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
+#
+# Builds and uploads conda packages for the current Python version and
+# all supported platforms (osx, win, linux).
 
-VERSION=`python setup.py --version`
+set -e
+
+cd /tmp
 
 # Setup Miniconda...
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p /tmp/miniconda
+export PATH="/tmp/miniconda/bin:$PATH"
 
 # Configure and update...
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels dansondergaard
 conda update -q conda
-conda install anaconda-client
-
-cd /tmp
+conda install anaconda-client conda-build
 
 # Build skeleton from PyPI package...
-conda skeleton pypi --pypi-url https://testpypi.python.org/pypi gwf --version $VERSION
-conda build --python 3.4 gwf/
-conda convert --platform all $HOME/miniconda/conda-bld/linux-64/gwf-$VERSION-py34_0.tar.bz2
+conda skeleton pypi --pypi-url $PYPI_URL gwf
+conda build --python $TRAVIS_PYTHON_VERSION gwf/
+mkdir /tmp/builds
+conda convert --platform all /tmp/miniconda/conda-bld/linux-64/gwf-*-*_0.tar.bz2 -o /tmp/builds
 
-for f in `ls */*.tar.bz2`; do
-    anaconda upload $f
+for f in `ls /tmp/builds/*/*.tar.bz2`; do
+    anaconda -t $ANACONDA_TOKEN upload $f
 done
