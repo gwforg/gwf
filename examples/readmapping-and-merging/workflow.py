86af2d4870ff28a17d8b2544c695c4ee1dcc946c
Dan Søndergaard <dan1990@gmail.com>
2017-01-20 15:14:12 +0100

    First update to examples.

--- examples/readmapping-and-merging/workflow.py
+++ examples/readmapping-and-merging/workflow.py
@@ -1,27 +1,29 @@
 
 # This is an example workflow for read mapping using bwa and samtools.
 
-from gwf import *
+from gwf import Workflow, template
+
+gwf = Workflow()
 
 
 # Templates
-unzip = template(input='{refGenome}.fa.gz', output='{refGenome}.fa') << '''
+unzip = template(inputs='{refGenome}.fa.gz', outputs='{refGenome}.fa') << '''
 zcat {refGenome}.fa.gz > {refGenome}.fa
 '''
 
-bwa_index = template(input='{refGenome}.fa', 
-                     output=['{refGenome}.amb', 
+bwa_index = template(inputs='{refGenome}.fa',
+                     outputs=['{refGenome}.amb',
                              '{refGenome}.ann', 
                              '{refGenome}.pac']) << '''
 
 bwa index -p {refGenome} -a bwtsw {refGenome}.fa
 '''
 
-bwa_map = template(input=['{R1}', '{R2}', 
+bwa_map = template(inputs=['{R1}', '{R2}',
                           '{refGenome}.amb', 
                           '{refGenome}.ann', 
                           '{refGenome}.pac'],
-                   output='{bamfile}',
+                   outputs='{bamfile}',
                    cores=16) << '''
 
 bwa mem -t 16 {refGenome} {R1} {R2} | \
@@ -50,14 +52,14 @@ R1files = ['Masala_{}_R1.fastq.gz'.format(i) for i in range(1,3)]
 R2files = ['Masala_{}_R2.fastq.gz'.format(i) for i in range(1,3)]
 bamfiles = ['Masala_{}.sorted.rmdup.bam'.format(i) for i in range(1,3)]
 
-target('UnZipGenome', walltime="01:00:00") << unzip(refGenome='ponAbe2')
-target('IndexGenome', walltime="01:00:00") << bwa_index(refGenome='ponAbe2')
+gwf.target('UnZipGenome', walltime="01:00:00") << unzip(refGenome='ponAbe2')
+gwf.target('IndexGenome', walltime="01:00:00") << bwa_index(refGenome='ponAbe2')
 
 for i in range(1,3):
-	target('MapReads_{}'.format(i), walltime="01:00:00") << \
+	gwf.target('MapReads_{}'.format(i), walltime="01:00:00") << \
 		bwa_map(refGenome='ponAbe2', 
 				R1='Masala_{}_R1.fastq.gz'.format(i), 
 				R2='Masala_{}_R2.fastq.gz'.format(i),
 		 		bamfile='Masala_{}.sorted.rmdup.bam'.format(i))
 
-target('Merge', walltime="01:00:00") << merge(individual='Masala')
+gwf.target('Merge', walltime="01:00:00") << merge(individual='Masala')
