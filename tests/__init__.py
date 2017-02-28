e8f21f48ec61fec4036699ad9da40eabfd22c163
Dan Søndergaard <dan1990@gmail.com>
2017-02-28 11:26:15 +0100

    Remove unused function.

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,16 +11,3 @@ class GWFTestCase(unittest.TestCase):
         thing = patcher.start()
         self.addCleanup(patcher.stop)
         return thing
-
-
-def create_test_target(name='TestTarget', inputs=None, outputs=None, options=None, workflow=None):
-    """A factory for `Target` objects."""
-    if inputs is None:
-        inputs = []
-    if outputs is None:
-        outputs = []
-    if options is None:
-        options = {}
-    if workflow is None:
-        workflow = Workflow(working_dir='/some/path')
-    return Target(name, inputs, outputs, options, workflow)
