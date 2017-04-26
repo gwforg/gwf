2b39018e5eceaaa976d6bf70541cb4e5d8d3b730
Dan Søndergaard <dan1990@gmail.com>
2017-04-26 13:42:26 +1000

    Fix logs plugin.

--- gwf/plugins/logs.py
+++ gwf/plugins/logs.py
@@ -1,47 +1,21 @@
-import logging
+import click
 
-from . import Plugin
+from ..cli import pass_graph, pass_backend
 from ..exceptions import TargetDoesNotExistError
 
-logger = logging.getLogger(__name__)
 
+@click.command()
+@click.argument('target')
+@click.option('-o/-e', '--stdout/--stderr')
+@pass_graph
+@pass_backend
+def logs(backend, graph, target, stdout):
+    """Display logs for the latest run of a target.
+    
+    By default only standard output is shown. Supply the --stderr flag to show standard error instead.
+    """
+    if target not in graph.targets:
+        raise TargetDoesNotExistError(target)
 
-class LogsCommand(Plugin):
-
-    help_text = (
-        'Display logs for the latest run of a target. By default only '
-        'standard output is shown. Supply the --stderr flag to show standard '
-        'error instead.'
-    )
-
-    def setup_argument_parser(self, parser, subparsers):
-        subparser = self.setup_subparser(
-            subparsers,
-            'logs',
-            self.help_text,
-            self.on_run
-        )
-
-        subparser.add_argument(
-            '-e',
-            '--stderr',
-            action='store_true',
-            help='Return stderr.',
-        )
-
-        subparser.add_argument(
-            'target',
-            help='Name of target.',
-        )
-
-    def on_run(self):
-        workflow = self.get_graph()
-        backend = self.get_active_backend()
-
-        target_name = self.config['target']
-        if target_name not in workflow.targets:
-            raise TargetDoesNotExistError(target_name)
-
-        target = workflow.targets[target_name]
-        log = backend.logs(target, stderr=self.config['stderr'])
-        print(log.read(), end='')
+    log = backend.logs(graph.targets[target], stderr=not stdout)
+    click.echo(log.read())
