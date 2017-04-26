13a3a3d3fc168e34acabbfe10283ef201dbca90f
Dan Søndergaard <dan1990@gmail.com>
2017-04-26 16:21:51 +1000

    Add test for run command.

---
+++ tests/plugins/test_run.py
@@ -0,0 +1,52 @@
+from unittest.mock import patch
+
+from tests import CliTestCase
+from gwf.cli import main
+
+
+SIMPLE_WORKFLOW = """from gwf import Workflow
+
+gwf = Workflow()
+gwf.target('Target1', inputs=[], outputs=[]) << "echo hello world"
+gwf.target('Target2', inputs=[], outputs=[]) << "echo world hello"
+"""
+
+
+@patch('gwf.plugins.run.schedule_many')
+class TestRun(CliTestCase):
+
+    def write_workflow(self):
+        with open('workflow.py', 'w') as fileobj:
+            fileobj.write(SIMPLE_WORKFLOW)
+
+    def test_run_all_targets(self, mock_schedule_many):
+        with self.runner.isolated_filesystem():
+            self.write_workflow()
+
+            args = ['-b', 'testing', 'run']
+            self.runner.invoke(main, args)
+
+            (graph, backend, targets), kwargs = mock_schedule_many.call_args
+            self.assertEqual(len(targets), 2)
+            self.assertEqual({x.name for x in targets}, {'Target1', 'Target2'})
+
+    def test_run_specified_target(self, mock_schedule_many):
+        with self.runner.isolated_filesystem():
+            self.write_workflow()
+
+            args = ['-b', 'testing', 'run', 'Target1']
+            self.runner.invoke(main, args)
+
+            (graph, backend, targets), kwargs = mock_schedule_many.call_args
+            self.assertEqual(len(targets), 1)
+            self.assertEqual({x.name for x in targets}, {'Target1'})
+
+    def test_dry_run(self, mock_schedule_many):
+        with self.runner.isolated_filesystem():
+            self.write_workflow()
+
+            args = ['-b', 'testing', 'run', '--dry-run']
+            self.runner.invoke(main, args)
+
+            (graph, backend, targets), kwargs = mock_schedule_many.call_args
+            self.assertTrue(kwargs['dry_run'])
