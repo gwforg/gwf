b1e561e71d3f0cf2a82efca0521df062bfa32086
Dan Søndergaard <dan1990@gmail.com>
2017-05-21 15:23:24 +1000

    Hugely simplify config... by deleting it all.

--- gwf/conf.py
+++ gwf/conf.py
@@ -9,68 +9,7 @@ CONFIG_DEFAULTS = {
 }
 
 
-def tokenize_key(key):
-    return key.split('.')
-
-
-class Config:
-
-    def __init__(self, initial):
-        self._config = initial
-
-    def __getitem__(self, key):
-        tokens = tokenize_key(key)
-        value = self._config[tokens[0]]
-        for token in tokens[1:]:
-            if not isinstance(value, dict):
-                raise KeyError(key)
-            value = value[token]
-        return value
-
-    def __setitem__(self, key, new_value):
-        tokens = tokenize_key(key)
-        value = self._config
-        for token in tokens[:-1]:
-            if token not in value:
-                value[token] = {}
-            value = value[token]
-        value[tokens[-1]] = new_value
-
-    def __delitem__(self, key):
-        tokens = tokenize_key(key)
-        value = self._config
-        for token in tokens[:-1]:
-            value = value[token]
-        del value[tokens[-1]]
-
-    def get(self, key, default=None):
-        try:
-            return self[key]
-        except KeyError:
-            return default
-
-    def __len__(self):
-        return len(self._config)
-
-    def iterkeys(self):
-        def deepiter(dct, parents):
-            for key, value in dct.items():
-                new_parents = parents + [key]
-                key_path = '.'.join(new_parents)
-                if isinstance(value, dict):
-                    yield from deepiter(value, parents=new_parents)
-                else:
-                    yield key_path
-        yield from deepiter(self._config, parents=[])
-
-    def __iter__(self):
-        return self.iterkeys()
-
-    def __repr__(self):
-        return repr(self._config)
-
-
-class FileConfig(Config):
+class FileConfig(dict):
 
     def __init__(self, path, initial):
         super().__init__(initial)
@@ -79,7 +18,7 @@ class FileConfig(Config):
     def dump(self):
         """Dump the configuration to disk."""
         with open(self.path, 'w') as config_file:
-            json.dump(dict(self._config), config_file, indent=4, sort_keys=True)
+            json.dump(self, config_file, indent=4, sort_keys=True)
 
     @classmethod
     def load(cls, path):
@@ -101,4 +40,4 @@ class FileConfig(Config):
 
 
 file_config = FileConfig.load('.gwfconf.json')
-config = ChainMap(file_config, Config(CONFIG_DEFAULTS))
+config = ChainMap(file_config, CONFIG_DEFAULTS)
