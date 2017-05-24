0fa23310cea80ef156c9c8204419c6743c21f7ff
Dan Søndergaard <dan1990@gmail.com>
2017-05-24 15:41:52 +1000

    Clean up travis.yml and conda build script a bit.

--- build_conda.sh
+++ build_conda.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 #
-# Builds and uploads conda packages for the current Python version and
-# all supported platforms (osx, win, linux).
+# Upload conda packages for the current Python version and all supported platforms (osx, win, linux).
+#
+# Author:
+#   Dan Søndergaard <das@birc.au.dk>
+#
 
-set -e
+set -eu
+set -o pipefail
 
 conda convert --platform all $HOME/conda-bld/linux-64/gwf-*-*_0.tar.bz2 -o conda-bld
 anaconda -u gwforg -t $ANACONDA_TOKEN upload conda-bld/*/*.tar.bz2
