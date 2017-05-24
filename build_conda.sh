fbbe654d35d5c39a3bc4185e2b7978e418d8b26a
Dan Søndergaard <dan1990@gmail.com>
2017-05-24 15:20:15 +1000

    Build and test package with conda.

--- build_conda.sh
+++ build_conda.sh
@@ -5,25 +5,5 @@
 
 set -e
 
-cd /tmp
-
-# Setup Miniconda...
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p /tmp/miniconda
-export PATH="/tmp/miniconda/bin:$PATH"
-
-# Configure and update...
-conda config --set always_yes yes --set changeps1 no
-conda config --add channels gwforg
-conda update -q conda
-conda install anaconda-client conda-build
-
-# Build skeleton from PyPI package...
-conda skeleton pypi --pypi-url $PYPI_URL gwf
-conda build --python $TRAVIS_PYTHON_VERSION gwf/
-mkdir /tmp/builds
-conda convert --platform all /tmp/miniconda/conda-bld/linux-64/gwf-*-*_0.tar.bz2 -o /tmp/builds
-
-for f in $(ls /tmp/builds/*/*.tar.bz2); do
-    anaconda -u gwforg -t $ANACONDA_TOKEN upload $f
-done
+conda convert --platform all $HOME/conda-bld/linux-64/gwf-*-*_0.tar.bz2 -o conda-bld
+anaconda -u gwforg -t $ANACONDA_TOKEN upload conda-bld/*/*.tar.bz2
