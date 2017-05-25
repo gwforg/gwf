e4cfe11d8cd45a9a10cecd87b7fe08f7e5120307
Dan Søndergaard <dan1990@gmail.com>
2017-05-25 10:56:37 +1000

    Remove versioneer and source version from gwf/__init__.py instead.

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
+import io
 
 from setuptools import find_packages, setup
 
-import versioneer
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
 
 
-# Utility function to read the README file.  Used for the
-# long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put
-# a raw string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name='gwf',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    version=find_version('gwf', '__init__.py'),
     packages=find_packages(),
     entry_points={
         'console_scripts': [
