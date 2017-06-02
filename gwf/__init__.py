4d0636c156ddc3584c099efa3cef25b286913d0a
Dan Søndergaard <dan1990@gmail.com>
2017-06-02 10:13:15 +1000

    Bump version.

--- gwf/__init__.py
+++ gwf/__init__.py
@@ -1,5 +1,5 @@
 from .core import Graph, Target, Workflow, schedule_many, schedule
 
-__version__ = '1.0b9'
+__version__ = '1.0b10'
 
 __all__ = ('Target', 'Workflow', 'Graph', 'schedule_many', 'schedule',)
