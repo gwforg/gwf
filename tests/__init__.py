08e4cbb7ebb56b7820ea2d5b8a9a4a4926fee4bf
Dan Søndergaard <dan1990@gmail.com>
2017-10-17 08:22:46 +0200

    Remove unused testing helpers.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,35 +1,5 @@
-from unittest import TestCase
-from unittest.mock import patch
-
-from click.testing import CliRunner
-
 from gwf.backends import Backend, Status
 from gwf.exceptions import LogNotFoundError
-
-
-def touch_file(path, contents=None):
-    with open(path, 'w') as fileobj:
-        if contents is not None:
-            fileobj.write(contents)
-
-
-class GWFTestCase(TestCase):
-
-    def create_patch(self, name, **kwargs):
-        patcher = patch(name, **kwargs)
-        thing = patcher.start()
-        self.addCleanup(patcher.stop)
-        return thing
-
-
-class CliTestCase(TestCase):
-
-    def setUp(self):
-        self.runner = CliRunner()
-
-        fs = self.runner.isolated_filesystem()
-        fs.__enter__()
-        self.addCleanup(fs.__exit__, None, None, None)
 
 
 class MockBackend(Backend):
