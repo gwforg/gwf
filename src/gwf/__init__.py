984c4f54327e9d43465b288052d250df00a34ba1
Dan Søndergaard <dan1990@gmail.com>
2018-04-30 12:55:47 +0200

    WIP: Yet another test...

--- src/gwf/__init__.py
+++ src/gwf/__init__.py
@@ -1,5 +1,5 @@
 from .core import Graph, Target, AnonymousTarget, Workflow, Scheduler
 
-__version__ = '1.3.0.dev3'
+__version__ = '1.3.0.dev4'
 
 __all__ = ('Graph', 'Target', 'AnonymousTarget', 'Workflow', 'Scheduler',)
