c2406478d9433e356ad268bf0ad6f2e68cd4bddb
Dan Søndergaard <dan1990@gmail.com>
2018-05-01 15:43:57 +0200

    Increment version.

--- src/gwf/__init__.py
+++ src/gwf/__init__.py
@@ -1,5 +1,5 @@
 from .core import Graph, Target, AnonymousTarget, Workflow, Scheduler
 
-__version__ = '1.3.0.dev6'
+__version__ = '1.3.0.dev7'
 
 __all__ = ('Graph', 'Target', 'AnonymousTarget', 'Workflow', 'Scheduler',)
