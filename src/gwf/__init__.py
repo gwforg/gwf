6bee1d7538368c2b03886e81bca1202f75ff1c24
Dan Søndergaard <dan1990@gmail.com>
2018-05-01 11:32:12 +0200

    Try dev release again.

--- src/gwf/__init__.py
+++ src/gwf/__init__.py
@@ -1,5 +1,5 @@
 from .core import Graph, Target, AnonymousTarget, Workflow, Scheduler
 
-__version__ = '1.3.0.dev5'
+__version__ = '1.3.0.dev6'
 
 __all__ = ('Graph', 'Target', 'AnonymousTarget', 'Workflow', 'Scheduler',)
