d81f143f114163c26182d2d8ec45faddfe3af433
Dan Søndergaard <dan1990@gmail.com>
2018-05-01 11:30:50 +0200

    Show how much data will be cleaned.
    Closes #230.

--- src/gwf/plugins/clean.py
+++ src/gwf/plugins/clean.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import os.path
 
 from ..core import graph_from_config
 from ..filtering import NameFilter, EndpointFilter, filter_generic
@@ -9,11 +10,22 @@ import click
 logger = logging.getLogger(__name__)
 
 
+def _format_size(num, suffix='B'):
+    # Implementation taken from:
+    # https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
+
 def _delete_file(path):
     try:
         os.remove(path)
     except OSError:
-        pass
+        msg = 'Error when attempting to delete {}, maybe the file did not exist'
+        logger.debug(msg.format(path))
 
 
 @click.command()
@@ -23,8 +35,9 @@ def _delete_file(path):
 def clean(obj, targets, all):
     """Clean output files of targets.
 
-    By default, only targets that are not endpoints will have their output files deleted. If you want to clean up output
-    files from endpoints too, use the ``--all`` flag.
+    By default, only targets that are not endpoints will have their output files 
+    deleted. If you want to clean up output files from endpoints too, use the 
+    ``--all`` flag.
     """
     graph = graph_from_config(obj)
 
@@ -34,9 +47,18 @@ def clean(obj, targets, all):
     if not all:
         filters.append(EndpointFilter(endpoints=graph.endpoints(), mode='exclude'))
 
-    matches = filter_generic(targets=graph, filters=filters)
+    matches = list(filter_generic(targets=graph, filters=filters))
+
+    total_size = sum(
+        os.path.getsize(path) if os.path.exists(path) else 0
+        for target in matches
+        for path in target.outputs
+    )
+
+    logger.info('Will delete %s of files!', _format_size(total_size))
+
     for target in matches:
-        logger.info('Deleting %s', target.name)
+        logger.info('Deleting output files of %s', target.name)
         for path in target.outputs:
-            logging.info('Deleting output file "%s" from target "%s"', click.format_filename(path), target.name)
+            logging.info('Deleting file "%s" from target "%s"', click.format_filename(path), target.name)
             _delete_file(path)
