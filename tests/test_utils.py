3c18a8322d0cd2e01bfe77555ee9f842f1d54d0f
Dan Søndergaard <das@birc.au.dk>
2018-05-30 10:54:24 +0200

    Rewrite tests for cache decorator to use pytest.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,22 +6,19 @@ import pytest
 from gwf.utils import parse_path, cache, PersistableDict
 
 
-class TestCache(unittest.TestCase):
-
-    def test_returns_same_object_when_called_twice_with_same_args(self):
-        func = cache(lambda x: object())
-        obj1 = func(42)
-        obj2 = func(42)
-        self.assertEqual(id(obj1), id(obj2))
-
-    def test_does_not_return_same_object_when_called_with_diff_args(self):
-        func = cache(lambda x: object())
-        obj1 = func(42)
-        obj2 = func(43)
-        self.assertNotEqual(id(obj1), id(obj2))
+def test_cache_returns_same_object_when_called_twice_with_same_args():
+    func = cache(lambda x: object())
+    obj1 = func(42)
+    obj2 = func(42)
+    assert id(obj1) == id(obj2)
 
 
-class TestParsePath(unittest.TestCase):
+def test_cache_does_not_return_same_object_when_called_with_diff_args():
+    func = cache(lambda x: object())
+    obj1 = func(42)
+    obj2 = func(43)
+    assert id(obj1) != id(obj2)
+
 
 @pytest.mark.parametrize("path,parsed_path", [
     ('/some/dir/workflow.py', ('/some/dir', 'workflow', 'gwf')),
