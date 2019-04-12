192f6999e4b54ea7e98e91d6edbae14053e49b4f
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-12 13:06:26 -0700

    Fixed PYTHON_W293 issues in src/gwf/plugins/touch.py

--- src/gwf/plugins/touch.py
+++ src/gwf/plugins/touch.py
@@ -12,11 +12,11 @@ def touchfile(path):
 @click.pass_obj
 def touch(obj):
     """Touch output files to update timestamps.
-    
+
     Running this command touches all output files in the workflow such that
     their modification timestamp is updated. Touching is performed bottom-up
     such that, when done, all targets in the workflow will look completed.
-    
+
     This is useful if one or more files were accidentially deleted, but you
     don't want to re-run the workflow to recreate them.
     """
