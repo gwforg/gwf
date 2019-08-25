e0086eb221e1786524c87fb713e31f92e6df9b41
Dan Søndergaard <dan1990@gmail.com>
2019-08-25 12:41:48 +0200

    Bump version to 1.6.0

--- src/gwf/__init__.py
+++ src/gwf/__init__.py
@@ -1,5 +1,5 @@
 from .core import Graph, Target, AnonymousTarget, Workflow, Scheduler
 
-__version__ = "1.5.1"
+__version__ = "1.6.0"
 
 __all__ = ("Graph", "Target", "AnonymousTarget", "Workflow", "Scheduler")
