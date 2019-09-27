9eadf813880bf145e8598ba921c531717e8dc291
Dan Søndergaard <das@birc.au.dk>
2019-09-27 13:08:06 +0200

    Use flattened_outputs() in touch command

--- src/gwf/plugins/touch.py
+++ src/gwf/plugins/touch.py
@@ -12,11 +12,11 @@ def touchfile(path):
 @click.pass_obj
 def touch(obj):
     """Touch output files to update timestamps.
-    
+
     Running this command touches all output files in the workflow such that
     their modification timestamp is updated. Touching is performed bottom-up
     such that, when done, all targets in the workflow will look completed.
-    
+
     This is useful if one or more files were accidentially deleted, but you
     don't want to re-run the workflow to recreate them.
     """
@@ -27,5 +27,5 @@ def touch(obj):
             if target in visited:
                 continue
             visited.add(target)
-            for path in target.outputs:
+            for path in target.flattened_outputs():
                 touchfile(path)
