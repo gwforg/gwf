53433e1b7d1833ace6b85bce85924268ca90e24e
Dan Søndergaard <das@birc.au.dk>
2019-10-03 13:45:05 +0200

    Bump version

--- src/gwf/__init__.py
+++ src/gwf/__init__.py
@@ -1,6 +1,6 @@
 from .core import Graph, Scheduler
 from .workflow import AnonymousTarget, Target, TargetList, Workflow
 
-__version__ = "1.6.0"
+__version__ = "1.7.0.dev0"
 
 __all__ = ("Graph", "Target", "AnonymousTarget", "Workflow", "TargetList", "Scheduler")
