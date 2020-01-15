1dad4d7d040b94511800774d944ae37428c48ddb
Dan Søndergaard <das@birc.au.dk>
2020-01-15 14:29:25 +0100

    Remove unused imports

--- docs/conf.py
+++ docs/conf.py
@@ -21,10 +21,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import os
-
-import sphinx_rtd_theme
-
 import gwf
 
 # -- General configuration ------------------------------------------------
