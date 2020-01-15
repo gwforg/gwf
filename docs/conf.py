6f3ca74e69525158f9bfcb793569ec1c7b37e78a
Dan Søndergaard <das@birc.au.dk>
2020-01-15 14:18:30 +0100

    Use the press theme

--- docs/conf.py
+++ docs/conf.py
@@ -136,8 +136,8 @@ todo_include_todos = False
 #
 # html_theme = 'alabaster'
 
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "press"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
