77d6cd99fbab86743a43ee44a10365ee91d93078
Dan Søndergaard <das@birc.au.dk>
2020-03-30 12:32:53 +0200

    Test on Python 3.8

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,7 @@ def setup_test_env(session):
     session.run("pip", "install", "--no-deps", "--editable", ".")
 
 
-@nox.session(python=["3.5", "3.6", "3.7"])
+@nox.session(python=["3.5", "3.6", "3.7", "3.8"])
 def test(session):
     setup_test_env(session)
 
