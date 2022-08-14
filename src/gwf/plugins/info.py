d8e192823f42bffe1c798cbe4a72b9ccd2eb7c41
Dan Søndergaard <das@genome.au.dk>
2022-08-14 16:40:37 +0200

    Add pretty output for info command

--- src/gwf/plugins/info.py
+++ src/gwf/plugins/info.py
@@ -8,6 +8,42 @@ from ..filtering import filter_names
 from ..workflow import Workflow
 
 
+def _indent(s):
+    return "    " + s
+
+
+def print_label(label):
+    click.secho(label, bold=True)
+
+
+def print_value(value, as_filename=False):
+    if as_filename:
+        value = click.format_filename(value)
+    click.secho(_indent(value))
+
+
+def print_list(values, **kwargs):
+    if not values:
+        print_value("-")
+    for value in values:
+        print_value(value, **kwargs)
+
+
+def print_pretty(targets, graph):
+    for target in targets:
+        print_label("Name:")
+        print_value(target.name)
+        print_label("Inputs:")
+        print_list(target.inputs, as_filename=True)
+        print_label("Outputs:")
+        print_list(target.outputs, as_filename=True)
+        print_label("Dependents:")
+        print_list([target.name for target in graph.dependents[target]])
+        print_label("Spec:")
+        print_list(target.spec.strip().splitlines())
+        click.secho()
+
+
 def print_json(targets, graph):
     obj = {}
     for target in targets:
@@ -30,12 +66,13 @@ def print_json(targets, graph):
 
 FORMATS = {
     "json": print_json,
+    "pretty": print_pretty,
 }
 
 
 @click.command()
 @click.argument("targets", nargs=-1)
-@click.option("-f", "--format", type=click.Choice(["json"]), default="json")
+@click.option("-f", "--format", type=click.Choice(["json", "pretty"]), default="json")
 @click.pass_obj
 def info(obj, targets, format):
     """Display information about a target."""
