9ee76a8b4f3aecf330ca84aee572a842a52f942e
Dan Søndergaard <das@genome.au.dk>
2023-01-21 13:45:00 +0100

    Fix status output
    Fixes #399.

--- src/gwf/plugins/status.py
+++ src/gwf/plugins/status.py
@@ -18,21 +18,6 @@ from ..workflow import Workflow
 logger = logging.getLogger(__name__)
 
 
-STATUS_COLORS = {
-    TargetStatus.SHOULDRUN: ("magenta", "⨯"),
-    TargetStatus.SUBMITTED: ("cyan", "-"),
-    TargetStatus.RUNNING: ("blue", "↻"),
-    TargetStatus.COMPLETED: ("green", "✓"),
-}
-
-STATUS_ORDER = (
-    TargetStatus.SHOULDRUN,
-    TargetStatus.SUBMITTED,
-    TargetStatus.RUNNING,
-    TargetStatus.COMPLETED,
-)
-
-
 def _status_str_to_enum(s):
     return TargetStatus[s.upper()]
 
@@ -41,10 +26,12 @@ def _status_strs_to_enums(iterable):
     return list(map(_status_str_to_enum, iterable))
 
 
-@attr.s
+@attr.define(frozen=True)
 class HumanStatus:
     status: TargetStatus = attr.ib()
-    reason: str = attr.ib()
+    reason: str = attr.ib(eq=False)
+    symbol: str = attr.ib()
+    color: str = attr.ib()
 
 
 def get_status(reason, backend):
@@ -58,70 +45,50 @@ def get_status(reason, backend):
     """
     status = backend.status(reason.target)
     if status == Status.RUNNING:
-        return HumanStatus(TargetStatus.RUNNING, "is running")
+        return HumanStatus(TargetStatus.RUNNING, "is running", symbol="↻", color="blue")
     elif status == Status.SUBMITTED:
-        return HumanStatus(TargetStatus.SUBMITTED, "has been submitted")
+        return HumanStatus(
+            TargetStatus.SUBMITTED, "has been submitted", symbol="-", color="cyan"
+        )
     elif reason.scheduled:
-        return HumanStatus(TargetStatus.SHOULDRUN, reason)
+        return HumanStatus(TargetStatus.SHOULDRUN, reason, symbol="⨯", color="magenta")
     else:
-        return HumanStatus(TargetStatus.COMPLETED, reason)
+        return HumanStatus(TargetStatus.COMPLETED, reason, symbol="✓", color="green")
 
 
-def print_table(graph, targets, reasons, backend):
+def print_table(graph, targets, target_status):
     targets = list(targets)
 
     name_col_width = max((len(target.name) for target in targets), default=0) + 4
-    format_str = (
-        "{status} {name:<{name_col_width}}{percentage:>7.2%}"
-        " [{num_shouldrun}/{num_submitted}/{num_running}/{num_completed}] {explanation}"
-    )
+    format_str = "{status} {name:<{name_col_width}}{percentage:>7.2%}    {explanation}"
 
     for target in sorted(targets, key=lambda t: t.order):
-        reason = reasons[target]
-        status = get_status(reason, backend)
-        color, symbol = STATUS_COLORS[status.status]
-
+        status = target_status[target]
         deps = graph.dfs(target)
-        deps_total = len(deps)
-
-        def num_deps_with_status(status):
-            return sum(1 for _ in deps if get_status(reason, backend) == status)
-
-        num_shouldrun = num_deps_with_status(TargetStatus.SHOULDRUN)
-        num_submitted = num_deps_with_status(TargetStatus.SUBMITTED)
-        num_running = num_deps_with_status(TargetStatus.RUNNING)
-        num_completed = num_deps_with_status(TargetStatus.COMPLETED)
-
-        percentage = num_completed / deps_total
-
+        percentage = sum(
+            1 for t in deps if target_status[t].status == TargetStatus.COMPLETED
+        ) / len(deps)
         line = format_str.format(
             name=target.name,
-            status=symbol,
+            status=status.symbol,
             percentage=percentage,
-            num_shouldrun=num_shouldrun,
-            num_submitted=num_submitted,
-            num_running=num_running,
-            num_completed=num_completed,
             name_col_width=name_col_width,
             explanation=status.reason or "",
         )
-        click.echo(click.style(line, fg=color))
+        click.secho(line, fg=status.color)
 
 
-def print_summary(_, targets, reasons, backend):
-    status_counts = Counter(
-        get_status(reasons[target], backend).status for target in targets
-    )
-    click.echo("∑ {:<15}{:>10}".format("total", len(targets)))
-    for status in STATUS_ORDER:
-        color, symbol = STATUS_COLORS[status]
-        padded_name = "{:<15}".format(status.name.lower())
-        click.echo(
-            "{} {}{:>10}".format(
-                click.style(symbol, fg=color),
-                click.style(padded_name, fg=color),
-                status_counts[status],
-            )
+def print_summary(_, targets, target_status):
+    status_counts = Counter(status for status in target_status.values())
+    click.echo("∑ {:<10}{:>10}".format("total", len(targets)))
+    for status, count in status_counts.items():
+        click.secho(
+            "{} {:<10}{:>10}".format(
+                status.symbol,
+                status.status.name.lower(),
+                count,
+            ),
+            fg=status.color,
         )
 
 
@@ -178,18 +145,24 @@ def status(obj, status, endpoints, format, targets):
 
     with backend_cls() as backend:
         reasons = schedule_workflow(
-            graph,
-            fs=CachedFilesystem(),
-            spec_hashes=spec_hashes,
+            graph, fs=CachedFilesystem(), spec_hashes=spec_hashes
         )
 
+        target_status = {}
+        for target, reason in reasons.items():
+            target_status[target] = get_status(reason, backend)
+
         def status_provider(target):
-            return get_status(reasons[target], backend).status
+            return target_status[target].status
 
         filters = []
         if status:
-            status = _status_strs_to_enums(status)
-            filters.append(StatusFilter(status_provider=status_provider, status=status))
+            filters.append(
+                StatusFilter(
+                    status_provider=status_provider,
+                    status=_status_strs_to_enums(status),
+                )
+            )
         if targets:
             filters.append(NameFilter(patterns=targets))
         if endpoints:
@@ -198,4 +171,4 @@ def status(obj, status, endpoints, format, targets):
         matches = filter_generic(targets=graph, filters=filters)
 
         printer = FORMATS[format]
-        printer(graph, matches, reasons, backend)
+        printer(graph, matches, target_status)
