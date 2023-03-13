60c346a77a495b0cb8d6d29db543a18739d2e32f
Dan Søndergaard <das@genome.au.dk>
2023-03-13 20:14:31 +0100

    Try to automatically create GitHub Release

---
+++ extract-changelog.py
@@ -0,0 +1,15 @@
+import sys
+
+ver = sys.argv[1]
+
+line = sys.stdin.readline()
+while not line.startswith("Version {}".format(ver)):
+    line = sys.stdin.readline()
+
+line = sys.stdin.readline()
+line = sys.stdin.readline()
+line = sys.stdin.readline()
+
+while not line.startswith("Version"):
+    print(line, end="")
+    line = sys.stdin.readline()
