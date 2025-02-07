8a9617fae71b1bac1d5ba268aea2b5b94f4aa25d
Dan Søndergaard <das@genome.au.dk>
2025-02-07 13:28:37 +0100

    Bump version

--- src/gwf/__init__.py
+++ src/gwf/__init__.py
@@ -3,6 +3,6 @@
 from .core import AnonymousTarget, Target
 from .workflow import TargetList, Workflow
 
-__version__ = "2.0.5"
+__version__ = "2.1.0"
 
 __all__ = ("Target", "AnonymousTarget", "Workflow", "TargetList")
