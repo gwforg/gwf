841b3b29f11ae32d36cc638f08b6ccfd64d8530c
Dan Søndergaard <das@genome.au.dk>
2025-02-07 11:14:40 +0100

    Remove old/confusing example workflows

--- examples/simple-workflow/workflow.py
+++
@@ -1,5 +0,0 @@
-from gwf import Workflow
-
-gwf = Workflow()
-gwf.target('Target1', inputs=[], outputs=['a.txt']) << "echo hello world"
-gwf.target('Target2', inputs=[], outputs=['b.txt']) << "echo world hello"
