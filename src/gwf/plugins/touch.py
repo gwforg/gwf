672a9efa1fb80ec0504a54b669b9a2a0aeb8b398
Dan Søndergaard <das@genome.au.dk>
2025-09-10 14:57:28 +0200

    Improve the touch command
    The touch command should either:
    
        only touch files that exist, or
        touch all files and potentially create the necessary directories.
    
    This should probably be controlled by a flag.

--- src/gwf/plugins/touch.py
+++ src/gwf/plugins/touch.py
@@ -8,7 +8,7 @@ from ..core import CachedFilesystem, Graph, get_spec_hashes, pass_context
 from ..filtering import filter_names
 
 
-def touch_workflow(endpoints, graph, spec_hashes):
+def touch_workflow(endpoints, graph, spec_hashes, create_missing):
     @lru_cache(maxsize=None)
     def _visit(target):
         for dep in graph.dependencies[target]:
@@ -16,7 +16,10 @@ def touch_workflow(endpoints, graph, spec_hashes):
 
         spec_hashes.update(target)
         for path in target.flattened_outputs():
-            Path(path).touch(exist_ok=True)
+            path = Path(path)
+            if path.exists() or create_missing:
+                path.parent.mkdir(parents=True, exist_ok=True)
+                path.touch(exist_ok=True)
 
     for target in endpoints:
         _visit(target)
@@ -24,21 +27,25 @@ def touch_workflow(endpoints, graph, spec_hashes):
 
 @click.command()
 @click.argument("targets", nargs=-1)
+@click.option("-c", "--create-missing", is_flag=True, default=False)
 @pass_context
-def touch(ctx, targets):
+def touch(ctx, targets, create_missing):
     """Touch output files to update timestamps.
 
-    Running this command touches all output files in the workflow such that
-    their modification timestamp is updated. Touching is performed bottom-up
-    such that, when done, all targets in the workflow will look completed. Spec
-    hashes will also be "touched".
+    Running this command touches all, existing output files in the workflow such
+    that their modification timestamp is updated. Touching is performed
+    bottom-up such that, when done, all targets in the workflow will look
+    completed. Spec hashes will also be "touched".
 
     This is useful if one or more files were accidentially deleted, but you
-    don't want to re-run the workflow to recreate them.
+    don't want to re-run the workflow to re-create them.
+
+    By default, only files that already exist will be touched/updated. If `-c`
+    is given, missing files will also be created.
     """
     workflow = Workflow.from_context(ctx)
     filesystem = CachedFilesystem()
     graph = Graph.from_targets(workflow.targets, filesystem)
     endpoints = filter_names(graph, targets) if targets else graph.endpoints()
     with get_spec_hashes(working_dir=ctx.working_dir, config=ctx.config) as spec_hashes:
-        touch_workflow(endpoints, graph, spec_hashes)
+        touch_workflow(endpoints, graph, spec_hashes, create_missing)
--- tests/plugins/test_touch.py
+++ tests/plugins/test_touch.py
@@ -4,8 +4,20 @@ import os.path
 from gwf.cli import main
 
 
-def test_touch_creates_files(cli_runner, local_backend, simple_workflow):
+def test_touch_creates_files_with_no_create_missing(
+    cli_runner, local_backend, simple_workflow
+):
     cli_runner.invoke(main, ["touch"])
+
+    assert not os.path.exists("a.txt")
+    assert not os.path.exists("b.txt")
+    assert not os.path.exists("c.txt")
+
+
+def test_touch_creates_files_with_create_missing(
+    cli_runner, local_backend, simple_workflow
+):
+    cli_runner.invoke(main, ["touch", "--create-missing"])
 
     assert os.path.exists("a.txt")
     assert os.path.exists("b.txt")
